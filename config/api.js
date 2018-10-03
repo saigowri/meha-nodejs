93972f3d0bfb15860c4db6dd699f4afed6c0a894
KavyaKPuranik <kpuranik08@gmail.com>
2018-10-03 12:26:46 +0530

    Session id

--- config/api.js
+++ config/api.js
@@ -1,4 +1,6 @@
 var apiai = require('apiai');
+var LocalStorage = require('node-localstorage').LocalStorage,
+localStorage = new LocalStorage('./scratch');
 
 // read the api.ai docs : https://api.ai/docs/
 
@@ -7,17 +9,17 @@ var app = apiai('4efec7cafaf24ce098001d038606e132');
 
 function getRandomInt(max) 
 {
-			  return Math.floor(Math.random() * Math.floor(max));
+	return Math.floor(Math.random() * Math.floor(max));
 }
 
 // Function which returns speech from api.ai
 var getRes = function(query) 
 {
-				/*var random1 = getRandomInt(100000);
+			var random1 = getRandomInt(100000);
 			var random2 = getRandomInt(100000);
 			var timestamp =  "" + parseInt(Date.now()) + random1 + random2;
 			var sessionId ;
-			if (typeof(Storage) !== "undefined") 
+			//if (typeof(Storage) !== "undefined") 
 			{
 				// Store
 				if(localStorage.getItem("sessionId")==null)
@@ -25,13 +27,12 @@ var getRes = function(query)
 					localStorage.setItem("sessionId", timestamp);
 				}
 				sessionId = localStorage.getItem("sessionId");
-			} 
+			} /*
 			else 
 			{
 				document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
 			}
 			*/
-	var sessionId = "test";
 	var request = app.textRequest(query, {
 					sessionId: sessionId
 	});
