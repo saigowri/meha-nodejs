3dc8fc7bd176ba25860408bea78cdfb327eb916c
KavyaKPuranik <kpuranik08@gmail.com>
2018-11-28 01:09:56 +0530

    Webhook

--- server/src/main/webhook.js
+++ server/src/main/webhook.js
@@ -1,4 +1,6 @@
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+var request = require('request');
+const xml = require("xml-parse");
 var connectWebhook = function(app)
 {
 app.use(bodyParser.json())
@@ -36,15 +38,51 @@ app.post('/webhook', function (req, res) {
   var action = req.body.queryResult.action;
   if(req.body.queryResult.hasOwnProperty('action') && req.body.queryResult.action.localeCompare('mental-health-info')==0)
   // parameters are stored in req.body.result.parameters 
-  var disease = req.body.queryResult.parameters['disease']
-  var webhookReply = 'Hi ' + disease + '! Welcome from the webhook.'
+  var disease = req.body.queryResult.parameters['disease'];
+  var webhookReply = "";
+  request('http://services.aonaware.com/DictService/DictService.asmx/DefineInDict?dictId=wn&word=depression', function (error, response, body) 
+	{
+		if (!error && response.statusCode == 200) 
+		{
+			var xmlBody = xml.parse(body);
+			for(var i in xmlBody)
+			{
+				if(xmlBody[i].hasOwnProperty('tagName') && xmlBody[i].tagName.localeCompare('WordDefinition')==0)
+				{
+						var wordDefinition = xmlBody[i].childNodes;
+						for(var j in wordDefinition)
+							if(wordDefinition[j].hasOwnProperty('tagName') &&  wordDefinition[j].tagName.localeCompare('Definitions')==0)
+							{
+									var definitions = wordDefinition[j].childNodes;
+									for(var k in definitions)
+										if(definitions[k].hasOwnProperty('tagName') && definitions[k].tagName.localeCompare('Definition')==0)
+										{
+											var definition = definitions[k].childNodes;
+											for(var l in definition)
+												if(definition[l].hasOwnProperty('tagName') && definition[l].tagName.localeCompare('WordDefinition')==0)
+												{
+													var webhookReply = definition[l].childNodes[0].text;
+													//webhookReply = webhookReply.replace(/"/g, "-");
+													//webhookReply = webhookReply.replace(/:/g, "-");
+													//webhookReply = webhookReply.replace(/{/g, "(");
+													//webhookReply = webhookReply.replace(/}/g, ")");
+													//webhookReply = webhookReply.replace(/\[/g, "(");
+													//webhookReply = webhookReply.replace(/]/g, ")");
+													//webhookReply = webhookReply.split("\n")[0];
+													console.log(webhookReply);
+													  res.status(200).json({
+														source: 'webhook-dm',
+														fulfillmentText: webhookReply
+													  })
+												}
+										}
+							}
+				}
+			}
+		}
+	});
+  
 
-  // the most basic response
-  res.status(200).json({
-    source: 'webhook',
-    speech: webhookReply,
-    displayText: webhookReply
-  })
 })
 };
 
