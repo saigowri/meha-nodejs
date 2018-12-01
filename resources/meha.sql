45cd643f771533b46f7c8a222ea2189d1d154765
KavyaKPuranik <kpuranik08@gmail.com>
2018-12-01 14:30:08 +0530

    Installation guide

--- resources/meha.sql
+++ resources/meha.sql
@@ -19,10 +19,6 @@
 -- Table structure for table `chatbot_details`
 --
 
-DROP DATABASE IF EXISTS mehaDB;
-CREATE DATABASE mehadb;
-USE mehadb;
-
 DROP TABLE IF EXISTS `chatbot_details`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
